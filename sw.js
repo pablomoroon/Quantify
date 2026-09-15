cd5260e111d1f3483ce2c3d64aab63c816dc8a60
pablomoroon <moronpablo05@gmail.com>
2026-09-15 09:47:29 +0200

    correccion

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'quantify-cache-v14';
+const CACHE_NAME = 'quantify-cache-v15';
 const CORE_ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './favicon.ico', './favicon-32.png', './favicon-16.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', (event) => {
