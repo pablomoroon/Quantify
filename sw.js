f3aa81e4dda0b805b56ce2d2bb5feb694e103fbb
pablomoroon <moronpablo05@gmail.com>
2026-09-16 12:09:35 +0200

    peer to peer

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'quantify-cache-v15';
+const CACHE_NAME = 'quantify-cache-v17';
 const CORE_ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './favicon.ico', './favicon-32.png', './favicon-16.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', (event) => {
